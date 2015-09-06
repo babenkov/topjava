d31d0cf0bf588e7080d429c271105f660d1a3107
babenkov <aababenkov@gmail.com>
2015-09-06 21:27:08 +0300

    Origins (start1-test)

--- src/main/java/ru/javawebinar/topjava/util/UserMealsUtil.java
+++ src/main/java/ru/javawebinar/topjava/util/UserMealsUtil.java
@@ -29,6 +29,7 @@ public class UserMealsUtil {
     }
 
     public static List<UserMealWithExceed> getFilteredMealsWithExceeded(List<UserMeal> mealList, LocalTime startTime, LocalTime endTime, int caloriesPerDay) {
+        //Test
         // TODO return filtered list with correctly exceeded field
         return null;
     }
